07cf6222b15a6c4f02ddb6564e6e537e74388c5a
RaviSriramChowdary <ravisriramchowdary@gmail.com>
2020-06-06 09:43:18 +0530

    Decorated the board using canvas Drawing.

--- scripts/index.js
+++ scripts/index.js
@@ -17,7 +17,7 @@ hasHomed = false;
 let whoseTurn = 0;
 let dieValue = 0;
 
-let numPlayers = document.getElementById("numPlayers").value;
+let numPlayers = 4;
 let numTokens = 4;
 
 let numSquares = 15;
@@ -80,7 +80,7 @@ const definePath = () => {
       }
    }
    for (let i = 0; i < pathLength; i++) {
-      if (pathSquares[i].isSafeSquare) console.log(i);
+      //if (pathSquares[i].isSafeSquare) ////console.log(i);
    }
 };
 
@@ -88,15 +88,18 @@ const addNext = (x, y, array) => {
    array.push({ x: x, y: y });
 };
 
+const colors = ["black", "#DC1924", "#2AA44A", "#FFDE16", "#0EABDE"];
 //sample codes here:
 
 let lockers = [
    { x: 2 * unit, y: 2 * unit },
-   { x: 2 * unit, y: 2 * unit },
-   { x: 7 * unit, y: 2 * unit },
-   { x: 7 * unit, y: 7 * unit },
-   { x: 2 * unit, y: 7 * unit },
+   { x: 1.5 * unit, y: 1.5 * unit },
+   { x: cvs.width / 2 + 0.5 * unit, y: 1.5 * unit },
+   { x: cvs.width / 2 + 0.5 * unit, y: cvs.width / 2 + 0.5 * unit },
+   { x: 1.5 * unit, y: cvs.width / 2 + 0.5 * unit },
 ];
+
+let lockWidth = cvs.width / 2 - 2 * unit;
 
 const drawToCvs = () => {
    //Drawing the path
@@ -104,58 +107,163 @@ const drawToCvs = () => {
    ctx.fillRect(0, 0, cvs.width, cvs.height);
 
    ctx.strokeStyle = "black";
+
    for (let i = 0; i < pathLength; i++) {
       ctx.fillStyle = "white";
-      if (i == 0) ctx.fillStyle = "Red";
-      if (i == numSquares - 1) ctx.fillStyle = "green";
-      if (i == 2 * numSquares - 2) ctx.fillStyle = "yellow";
-      if (i == 3 * numSquares - 3) ctx.fillStyle = "cyan";
+      if (i == 0) ctx.fillStyle = "#DC1924";
+      if (i == numSquares - 1) ctx.fillStyle = "#2AA44A";
+      if (i == 2 * numSquares - 2) ctx.fillStyle = "#FFDE16";
+      if (i == 3 * numSquares - 3) ctx.fillStyle = "#0EABDE";
       ctx.fillRect(gamePath[i].x, gamePath[i].y, unit, unit);
+
+      ctx.strokeStyle = "#00000010";
+      ctx.lineWidth = unit / 8;
+      let x = ctx.lineWidth;
+      ctx.strokeRect(
+         gamePath[i].x + x / 2,
+         gamePath[i].y + x / 2,
+         unit - x,
+         unit - x
+      );
+
+      ctx.strokeStyle = "#00000050";
+      ctx.lineWidth = 1;
+      ctx.strokeRect(
+         gamePath[i].x + x / 2,
+         gamePath[i].y + x / 2,
+         unit - x,
+         unit - x
+      );
+
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1;
       ctx.strokeRect(gamePath[i].x, gamePath[i].y, unit, unit);
 
       ctx.font = "18px Arial";
       ctx.fillStyle = "black";
       for (let j = 1; j < numTokens + 1; j++) {
          if (pathSquares[i].numTokensPlayer[j] > 0) {
-            if (j == 1)
-               ctx.fillText(
-                  pathSquares[i].numTokensPlayer[j] + "R",
-                  gamePath[i].x + 2,
-                  gamePath[i].y + 22
+            for (let l = 0; l < pathSquares[i].numTokensPlayer[j]; l++) {
+               drawToken(
+                  gamePath[i].x + unit / 2,
+                  gamePath[i].y + unit / 2,
+                  colors[j],
+                  1 / pathSquares[i].numTokensPlayer[j]
                );
-            if (j == 2)
-               ctx.fillText(
-                  pathSquares[i].numTokensPlayer[j] + "G",
-                  gamePath[i].x + unit - 26,
-                  gamePath[i].y + 22
-               );
-            if (j == 3)
-               ctx.fillText(
-                  pathSquares[i].numTokensPlayer[j] + "Y",
-                  gamePath[i].x + 2,
-                  gamePath[i].y + unit - 5
-               );
-            if (j == 4)
-               ctx.fillText(
-                  pathSquares[i].numTokensPlayer[j] + "B",
-                  gamePath[i].x + unit - 24,
-                  gamePath[i].y + unit - 5
-               );
+            }
          }
       }
    }
-   ctx.fillStyle = "red";
-   ctx.fillRect(2 * unit, 2 * unit, 3 * unit, 3 * unit);
-   ctx.strokeRect(2 * unit, 2 * unit, 3 * unit, 3 * unit);
-   ctx.fillStyle = "green";
-   ctx.fillRect(7 * unit, 2 * unit, 3 * unit, 3 * unit);
-   ctx.strokeRect(7 * unit, 2 * unit, 3 * unit, 3 * unit);
-   ctx.fillStyle = "yellow";
-   ctx.fillRect(7 * unit, 7 * unit, 3 * unit, 3 * unit);
-   ctx.strokeRect(7 * unit, 7 * unit, 3 * unit, 3 * unit);
-   ctx.fillStyle = "cyan";
-   ctx.fillRect(2 * unit, 7 * unit, 3 * unit, 3 * unit);
-   ctx.strokeRect(2 * unit, 7 * unit, 3 * unit, 3 * unit);
+   lockWidth = cvs.width / 2 - 2 * unit;
+   ctx.strokeStyle = "black";
+   for (let i = 1; i <= numPlayers; i++) {
+      switch (i) {
+         case 1:
+            ctx.fillStyle = "#DC1924";
+            break;
+         case 2:
+            ctx.fillStyle = "#2AA44A";
+            break;
+         case 3:
+            ctx.fillStyle = "#FFDE16";
+            break;
+         case 4:
+            ctx.fillStyle = "#0EABDE";
+            break;
+         default:
+            ctx.fillStyle = "black";
+            break;
+      }
+      ctx.fillRect(lockers[i].x, lockers[i].y, lockWidth, lockWidth);
+      ctx.strokeRect(lockers[i].x, lockers[i].y, lockWidth, lockWidth);
+
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "white";
+      if (whoseTurn == 0)
+         ctx.strokeRect(
+            lockers[1].x + unit / 4,
+            lockers[1].y + unit / 4,
+            lockWidth - unit / 2,
+            lockWidth - unit / 2
+         );
+      else
+         ctx.strokeRect(
+            lockers[whoseTurn].x + unit / 4,
+            lockers[whoseTurn].y + unit / 4,
+            lockWidth - unit / 2,
+            lockWidth - unit / 2
+         );
+
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "gold";
+      if (whoseTurn == 0)
+         ctx.strokeRect(
+            lockers[1].x + unit / 4,
+            lockers[1].y + unit / 4,
+            lockWidth - unit / 2,
+            lockWidth - unit / 2
+         );
+      else
+         ctx.strokeRect(
+            lockers[whoseTurn].x + unit / 4,
+            lockers[whoseTurn].y + unit / 4,
+            lockWidth - unit / 2,
+            lockWidth - unit / 2
+         );
+
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+
+      for (let j = 0; j < numTokens; j++) {
+         let centerX, centerY;
+         ctx.beginPath();
+         ctx.fillStyle = "white";
+         if (j == 0) {
+            centerX = lockers[i].x + lockWidth / 4;
+            centerY = lockers[i].y + lockWidth / 4;
+            ctx.arc(centerX, centerY, lockWidth / 8, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+
+            if (gamePlay[i].numTokenInLocker > j) {
+               drawToken(centerX, centerY, colors[i], 1);
+            }
+         }
+         if (j == 1) {
+            centerX = lockers[i].x + (3 * lockWidth) / 4;
+            centerY = lockers[i].y + lockWidth / 4;
+            ctx.arc(centerX, centerY, lockWidth / 8, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+
+            if (gamePlay[i].numTokenInLocker > j) {
+               drawToken(centerX, centerY, colors[i], 1);
+            }
+         }
+         if (j == 2) {
+            centerX = lockers[i].x + lockWidth / 4;
+            centerY = lockers[i].y + (3 * lockWidth) / 4;
+            ctx.arc(centerX, centerY, lockWidth / 8, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+
+            if (gamePlay[i].numTokenInLocker > j) {
+               drawToken(centerX, centerY, colors[i], 1);
+            }
+         }
+         if (j == 3) {
+            centerX = lockers[i].x + (3 * lockWidth) / 4;
+            centerY = lockers[i].y + (3 * lockWidth) / 4;
+            ctx.arc(centerX, centerY, lockWidth / 8, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+
+            if (gamePlay[i].numTokenInLocker > j) {
+               drawToken(centerX, centerY, colors[i], 1);
+            }
+         }
+      }
+   }
 
    ctx.font = unit + "px Arial";
    ctx.fillStyle = "black";
@@ -169,12 +277,30 @@ const drawToCvs = () => {
    );
 
    for (let k = 1; k <= numPlayers; k++) {
-      ctx.fillText(gamePlay[k].numTokenInLocker, lockers[k].x, lockers[k].y);
-   }
-   for (let k = 1; k <= numPlayers; k++) {
       ctx.fillText("Score: " + score[k], lockers[k].x + 30, lockers[k].y + 30);
    }
 };
+
+function drawToken(x, y, color, factor) {
+   ctx.strokeStyle = "black";
+   ctx.beginPath();
+   ctx.arc(x, y, (unit / 4) * factor, 0, 2 * Math.PI);
+   ctx.stroke();
+   ctx.fillStyle = color;
+   ctx.fill();
+
+   ctx.beginPath();
+   ctx.arc(x, y, (unit / 6) * factor, 0, 2 * Math.PI);
+   ctx.stroke();
+   ctx.fillStyle = "white";
+   ctx.fill();
+
+   ctx.beginPath();
+   ctx.arc(x, y, (unit / 10) * factor, 0, 2 * Math.PI);
+   ctx.stroke();
+   ctx.fillStyle = color;
+   ctx.fill();
+}
 
 const defineGamePlay = () => {
    for (let i = 1; i <= numPlayers; i++) {
@@ -201,6 +327,10 @@ const trackTokens = () => {
    }
 };
 
+const isThereAMove = () => {
+   
+}
+
 const scaleNum = (num, factor) => {
    let result;
    let corr = (factor - 1) * (numSquares - 1);
@@ -213,7 +343,7 @@ const scaleNum = (num, factor) => {
          result = num + corr;
       }
    }
-   console.log(result + " and corr is: " + corr);
+   ////console.log(result + " and corr is: " + corr);
    return result;
 };
 
@@ -227,7 +357,7 @@ const rand = (min, max) => min + Math.floor(Math.random() * (max - min + 0.7));
 document.getElementById("header").addEventListener(
    "click",
    function () {
-      console.log("canRoll: " + canRoll);
+      ////console.log("canRoll: " + canRoll);
       if (canRoll) rollDie();
       else {
          if (
@@ -241,14 +371,14 @@ document.getElementById("header").addEventListener(
    false
 );
 
-document.getElementById('goNext').addEventListener('click', function () {
-   document.getElementById("welcome").style.display = "none";
-   document.getElementById('selectorColor').style.display = "block";
-})
+// document.getElementById('goNext').addEventListener('click', function () {
+//    document.getElementById("welcome").style.display = "none";
+//    document.getElementById('selectorColor').style.display = "block";
+// })
 
-document.getElementById("startGame").addEventListener("click", function () {
-   document.getElementById("selectorColor").style.display = "none";
-});
+// document.getElementById("startGame").addEventListener("click", function () {
+//    document.getElementById("selectorColor").style.display = "none";
+// });
 
 function rollDie() {
    if (dieValue != 6 && !hasKilled && !hasHomed) {
@@ -266,6 +396,23 @@ const switchPlayer = () => {
    if (whoseTurn == numPlayers) {
       whoseTurn = 1;
    } else whoseTurn++;
+   // if (whoseTurn == 0) {
+   //    whoseTurn = 1;
+   // }
+   // else if (whoseTurn == 1) {
+   //    whoseTurn = 3;
+   // }
+   // else if (whoseTurn == 3) {
+   //    whoseTurn = 2;
+   // }
+
+   // else if (whoseTurn == 2) {
+   //    whoseTurn = 4;
+   // }
+
+   // else if (whoseTurn == 4) {
+   //    whoseTurn = 1;
+   // }
 };
 
 //number is 6 => no switch
@@ -286,13 +433,13 @@ function updateGame() {
       hasClickedon(
          lockers[whoseTurn].x,
          lockers[whoseTurn].y,
-         3 * unit,
-         3 * unit
+         lockWidth,
+         lockWidth
       ) &&
       hasMoved == false &&
       gamePlay[whoseTurn].numTokenInLocker > 0
    ) {
-      console.log("dimpadu");
+      //////console.log("dimpadu");
       gamePlay[whoseTurn].numTokenInLocker--;
       pathSquares[scaleNum(0, whoseTurn)].numTokensPlayer[whoseTurn]++;
       hasMoved = true;
@@ -322,7 +469,10 @@ function updateGame() {
          }
 
          for (let m = 1; m <= numPlayers; m++) {
-            if (m != whoseTurn && pathSquares[temp + dieValue].numTokensPlayer[m] > 0) {
+            if (
+               m != whoseTurn &&
+               pathSquares[temp + dieValue].numTokensPlayer[m] > 0
+            ) {
                gamePlay[m].numTokenInLocker +=
                   pathSquares[temp + dieValue].numTokensPlayer[m];
                pathSquares[temp + dieValue].numTokensPlayer[m] = 0;
