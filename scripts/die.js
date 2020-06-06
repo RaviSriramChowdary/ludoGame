742ad8b0ba8284e5bc283d46ac0c343f897e4d30
RaviSriramChowdary <ravisriramchowdary@gmail.com>
2020-06-06 09:46:09 +0530

    Disabled die for testing the game.

--- scripts/die.js
+++ scripts/die.js
@@ -53,4 +53,4 @@ function getRandomNumber(min, max) {
    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-document.getElementById("dieHolder").addEventListener("click", rollDice);
+// document.getElementById("dieHolder").addEventListener("click", rollDice);
